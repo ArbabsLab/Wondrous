8d6c74aadad3bcd97f8f00db65efcb1581cacfb8
Arbab <arbabhusain200@gmail.com>
2025-09-06 22:26:00 -0400

    Added authentication using clerk

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton mode="modal"/>
+      </SignedOut>
+            
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+}
